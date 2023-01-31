1461564f15a44f32e6554d638ccf30b427f63fa8
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-01-31 13:19:27 +0530

    Added products

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ProductsContext } from "../Global/Products.jsx";
 
 const Product = () => {
-  return (
-    <h1>Products</h1>
-  )
-}
+  const { products } = useContext(ProductsContext);
 
-export default Product
+  return (
+    <div id="products">
+      {products.map((product) => (
+        <div id="product" key={product.id}>
+          <div id="product_img">
+            <img src={product.image} alt="not found" />
+          </div>
+          <div id="prouct_details">
+            <div id="product_name">{product.name}</div>
+            <div id="product_price">${product.price}.00</div>
+          </div>
+          <div id="add_cart">Add to cart</div>
+          {product.status == 'hot'? <div className="hot"></div>: ''}
+          {product.status == 'new'? <div className="new"></div>: ''}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Product;

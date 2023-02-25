50e4337a81b9370abbd7b17da85dfbad89655dcb
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-02-25 22:07:42 +0530

    Completed all the functionalities

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,9 +1,11 @@
 import React, { useContext } from "react";
 import { CartContext } from "../Global/CartContext";
+import StripeCheckout from "react-stripe-checkout"
 
 const Cart = () => {
   const { shoppingCart, totalPrice, qty, dispatch } = useContext(CartContext);
- 
+  
+
   return (
     <div className="cart-container">
       <div className="cart_details" style={{ marginTop: "100px" }}>
@@ -25,6 +27,24 @@ const Cart = () => {
             ))
           : "Sorry your cart is currently empty."}
       </div>
+      {shoppingCart.length >0 ? <div className="cart_summary">
+        <div className="summary">
+          <h3>Cart Summary</h3>
+          <div className="TotalItems">
+            <div className="items">Total Items</div>
+            <div className="items_count">{qty}</div>
+          </div>
+          <div className="total_price_section">
+            <div className="just_title">Total Price</div>
+            <div className="items_price">Rs.{totalPrice}/-</div>
+          </div>
+          <div className="stripe_section">
+              <StripeCheckout stripeKry="pk_test_51MfQYFSG8ddIUAoLd8KLmytQ606jnrleIijDl3veNQst2MIgJB11dGfuF7mLlrZT2TP2GKqV97F5vVszdFmqXnPG00oZisiQkB">
+
+              </StripeCheckout>
+          </div>
+        </div>
+      </div> : ''}
     </div>
   );
 };
